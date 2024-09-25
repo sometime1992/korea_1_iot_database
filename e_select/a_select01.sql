dd01f8f6b8a44af671d026dab2405336b5dcdc8e
sometime1992 <sometime1992@naver.com>
2024-09-25 10:00:12 +0900

    20240925

--- e_select/a_select01.sql
+++ e_select/a_select01.sql
@@ -15,5 +15,23 @@
     6. order by 정령 컬럼명: 결과를 특정 컬럼의 순서로 정렬
     7. limit 컬럼 수 제한: 반환할 컬럼(행)의 수를 제한
     
+    cf) 데이터 베이스 내부 처리 순서
+    : select문 처리 순서
+    
+    from > join > where > group by > having > select > order by > limit
     
 */
+
+use `korea_db`;
+
+# 1. 기본 조회
+# : select 컬럼명 from 데이터베이스명.테이블명;
+
+select `name` from `korea_db`.`members`;
+# : 정렬을 하지 않을 경우 데이터 입력 순서대로 출력
+
+# cf) 전체 컬럼 조회
+# : 컬럼명 작성에 * (전체 선택)을 사용하여 조회
+select * from `korea_db`.`members`;
+
+
